d10a2dc0a5b47a6d403a385264f3697d85751ca2
GodSang <tkdgh584@gmail.com>
2021-04-11 22:39:12 +0900

    불필요 폴더 삭제

--- middle/index.js
+++ middle/index.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../capstonedesign-7b8c8-firebase-adminsdk-uyobr-a6f6472017.json");
+var serviceAccount = require("../capstonedesign-firebase-key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
