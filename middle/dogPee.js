3350b3810b3e61767b48e957e35060d545499a07
GodSang <tkdgh584@gmail.com>
2021-04-27 20:47:50 +0900

    pee data Read 및 Pagination 구현

---
+++ middle/dogPee.js
@@ -0,0 +1,58 @@
+var express = require('express');
+const db = require('../models');
+const Sequelize = require('sequelize');
+
+const createPeeData = async (req, res, next) => {
+    const eeData = req.body;
+
+    try {
+        await db.dog_pee.create({
+            size: peeData.size,
+            RGB: peeData.RGB,
+            user_dog_id: req.currentUser.uid
+        })
+        next();
+    } catch (e) {
+        console.log(e)
+        res.status(400).json({"message":"database insert error(Pee)"});
+    }
+}
+
+
+const readPeeData = async (req, res, next) => {
+    const peeData = req.query;
+    const Op = Sequelize.Op;
+    var startDate = new Date(peeData.date);
+    var endDate = new Date(peeData.date);
+    endDate.setDate(endDate.getDate()+1);
+
+    try {
+        const result =  await db.dog_pee.findAll({
+            attributes: [
+                "RGB", "createdAt"
+            ],
+            where: {
+                user_dog_id: req.currentUser.uid,
+                createdAt: {
+                    [Op.gt]: startDate,
+                    [Op.lt]: endDate
+                },
+            },
+            order: [
+                ["createdAt", "DESC"]
+            ],
+            limit: 2,
+            offset: peeData.page * 2,
+        })    
+        res.json(result);
+        //next();
+    } catch (e) {
+        console.log(e)
+        res.status(400).json({"message":"database insert error(Pee)"});
+    }
+}
+
+module.exports = {
+    createPeeData: createPeeData,
+    readPeeData: readPeeData,
+}
--- routes/pee.js
+++ routes/pee.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../models/index');
-const dogToRaspb = require('../middle/dogToRaspb');
 const auth = require('../middle/auth');
+const dogPee = require('../middle/dogPoo');
 
 // Create
-router.post('/', auth.setCurrentUser, auth.checkPermission, dogToRaspb.insertPeeData,
+router.post('/', auth.setCurrentUser, auth.checkPermission, dogPee.insertPeeData,
   function(req, res, next) {
     res.status(201).json({message: "insert pee success"});
+});
+
+router.get('/', auth.setCurrentUser, auth.checkPermission, dogPee.readPooData,
+  function(req, res, next) {
+    res.status(201).json({message: "read poo success"});
 });
 
 module.exports = router;
