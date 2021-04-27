c04fd46c4149bc2c8325913db4d36f71f868a5c1
GodSang <tkdgh584@gmail.com>
2021-04-27 20:56:18 +0900

    intake read 라우터 추가

--- routes/intake.js
+++ routes/intake.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../models/index');
-const dogToRaspb = require('../middle/dogToRaspb');
+const dogIntake = require('../middle/dogIntake');
 const auth = require('../middle/auth');
 
 // Create
-router.post('/', auth.setCurrentUser, auth.checkPermission, dogToRaspb.insertEatData,
+router.post('/', auth.setCurrentUser, auth.checkPermission, dogIntake.createEatData,
   function(req, res, next){
-    res.status(201).json({message: "insert intake success"});
+    res.status(201).json({message: "create intake success"});
 });
 
 // Read
-router.get('/', )
+router.get('/', auth.setCurrentUser, auth.checkPermission, dogIntake.readIntakeData,
+  function(req, res, next) {
+    res.status(201).json({message: "read intake success"});
+});
 
 module.exports = router;
