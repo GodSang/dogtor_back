9292d58858379c84d054a2ee0b003aebaf068719
GodSang <tkdgh584@gmail.com>
2021-04-28 17:18:36 +0900

    회원가입 response 수정

--- routes/signup.js
+++ routes/signup.js
@@ -14,8 +14,15 @@ router.post('/', async function (req, res, next) {
       dog_gender: userInfo.dog_gender,
       dog_weight: userInfo.dog_weight,
     });
-    res.status(200).json({ message: '가입 성공' });
+    res.status(200).json({
+      dog_name: userInfo.dog_name,
+      dog_birth: userInfo.dog_birth,
+      dog_type: userInfo.dog_type,
+      dog_gender: userInfo.dog_gender,
+      dog_weight: userInfo.dog_weight,
+    });
   } catch (e) {
+    res.json(e);
     res.status(401).json({ message: '중복되는 정보라 실패' });
   }
 });
