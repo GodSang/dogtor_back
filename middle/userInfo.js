aa17d9f4c10a5d289862b6f83f2b59eb0c0962df
GodSang <tkdgh584@gmail.com>
2021-04-28 23:09:28 +0900

    user_dog 정보 read 미들웨어 및 라우터 구현

--- middle/userInfo.js
+++ middle/userInfo.js
@@ -1,10 +1,11 @@
 const db = require('../models');
 const Sequelize = require('sequelize');
 
-const createuserInfo = async (req, res, next) => {
+const createUserInfo = async (req, res, next) => {
   const userInfo = req.body;
+
   try {
-    await db.user_dog_info.create({
+    const result = await db.user_dog_info.create({
       uid: userInfo.uid,
       dog_name: userInfo.dog_name,
       dog_birth: userInfo.dog_birth,
@@ -13,18 +14,34 @@ const createuserInfo = async (req, res, next) => {
       dog_weight: userInfo.dog_weight,
     });
 
-    res.status(200).json({
-      dog_name: userInfo.dog_name,
-      dog_birth: userInfo.dog_birth,
-      dog_type: userInfo.dog_type,
-      dog_gender: userInfo.dog_gender,
-      dog_weight: userInfo.dog_weight,
-    });
+    result.uid = null;
+    res.status(201).json(result);
   } catch (e) {
     res.status(401).json({ message: '회원 가입 실패' });
   }
 };
 
+const readUserInfo = async (req, res, next) => {
+  try {
+    const result = await db.user_dog_info.findOne({
+      attributes: [
+        'dog_name',
+        'dog_birth',
+        'dog_type',
+        'dog_gender',
+        'dog_weight',
+      ],
+      where: {
+        uid: req.currentUser.uid,
+      },
+    });
+    res.status(200).json(result);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json({ message: 'database insert error(Poo)' });
+  }
+};
 module.exports = {
-  createuserInfo: createuserInfo,
+  createUserInfo: createUserInfo,
+  readUserInfo: readUserInfo,
 };
--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,17 @@
 var express = require('express');
-const db = require('../models');
 var router = express.Router();
+const userInfo = require('../middle/userInfo');
+const auth = require('../middle/auth');
 
 // Create
-router.post('/', async function (req, res, next) {
-  const userInfo = req.body;
+router.post('/', userInfo.createUserInfo);
 
-  try {
-    const result = await db.user_dog_info.create({
-      uid: userInfo.uid,
-      dog_name: userInfo.dog_name,
-      dog_birth: userInfo.dog_birth,
-      dog_type: userInfo.dog_type,
-      dog_gender: userInfo.dog_gender,
-      dog_weight: userInfo.dog_weight,
-    });
-    res.status(200).json({
-      dog_name: userInfo.dog_name,
-      dog_birth: userInfo.dog_birth,
-      dog_type: userInfo.dog_type,
-      dog_gender: userInfo.dog_gender,
-      dog_weight: userInfo.dog_weight,
-    });
-  } catch (e) {
-    res.status(401).json({ message: '중복되는 정보라 실패' });
-  }
-});
+// Read
+router.get(
+  '/',
+  auth.setCurrentUser,
+  auth.checkPermission,
+  userInfo.readUserInfo
+);
 
 module.exports = router;
