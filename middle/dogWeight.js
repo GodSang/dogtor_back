1ab120b04e1efd6aa5dc99be2e435d12f41c3d19
GodSang <tkdgh584@gmail.com>
2021-05-13 03:52:29 +0900

    강아지 몸무게 READ 미들웨어 구현

--- middle/dogWeight.js
+++ middle/dogWeight.js
@@ -1,6 +1,19 @@
 const db = require('../models');
 
-const readWeightData = async (req, res, next) => {};
+const readWeightData = async (req, res, next) => {
+  try {
+    const result = await db.user_dog_info.findOne({
+      where: { uid: req.currentUser.uid },
+      attributes: ['dog_weight'],
+    });
+    req.currentUser = {
+      dog_weight: result.dataValues.dog_weight,
+    };
+    next();
+  } catch (e) {
+    res.status(400).json({ message: 'database read error(Weight)' });
+  }
+};
 
 const updateWeightData = async (req, res, next) => {
   const dogWeight = req.body.weight;
