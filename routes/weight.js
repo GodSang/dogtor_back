19ab52aebc6ae750434249405b2c4f8b6a16c0ff
GodSang <tkdgh584@gmail.com>
2021-05-13 03:51:35 +0900

    강아지 권장 몸무게 계산 라우터 구현

--- routes/weight.js
+++ routes/weight.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const auth = require('../middle/auth');
 const dogWeight = require('../middle/dogWeight');
+const dogFat = require('../middle/fatCalc');
 
 // Create
 router.post(
@@ -17,5 +18,16 @@ router.post(
 router.get('/', auth.setCurrentUser, function (req, res, next) {
   res.status(201).json({ message: 'read poo success' });
 });
+
+// 체지방량 Read
+router.get(
+  '/fat',
+  auth.setCurrentUser,
+  dogWeight.readWeightData,
+  dogFat.calculatePooData,
+  function (req, res, next) {
+    res.status(201).json({ message: 'read fat success' });
+  }
+);
 
 module.exports = router;
