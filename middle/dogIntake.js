1e1231adac74dfdff9f5db46b2ceb32f56ddc344
GodSang <tkdgh584@gmail.com>
2021-05-28 23:12:20 +0900

    권장 식사량 계산하는 미들웨어 추가

--- middle/dogIntake.js
+++ middle/dogIntake.js
@@ -41,13 +41,63 @@ const readIntakeData = async (req, res, next) => {
   }
 };
 
-const calcIntake = async (req, res, next) => {
-  req.shouldRunFcm = false;
+const calcIntakeSum = async (req, res, next) => {
+  const intakeData = req.query;
+  const Op = Sequelize.Op;
+  const recommendAmount = req.currentData.reAmount;
+
+  var startDate = new Date(intakeData.date);
+  var endDate = new Date(intakeData.date);
+  endDate.setDate(endDate.getDate() + 1);
+  const mealData = await db.intake.findAndCountAll({
+    attributes: ['amountOfMeal'],
+    where: {
+      user_dog_id: req.currentUser.uid,
+      createdAt: {
+        [Op.gte]: startDate,
+        [Op.lt]: endDate,
+      },
+    },
+  });
+  let amountOfMeal = mealData.rows;
+  let sumOfMeal = 0;
+
+  amountOfMeal.forEach(function (e) {
+    sumOfMeal += e.amountOfMeal;
+  });
+
+  sumOfMeal *= 4.2;
+  let recomMeal = { sumMeal: sumOfMeal, recommendMeal: recommendAmount };
+  res.status(201).json(recomMeal);
+};
+
+const calcRecommendIntake = async (req, res, next) => {
+  const weightData = await db.user_dog_info.findOne({
+    attributes: ['dog_weight', 'dog_gender'],
+    where: {
+      uid: req.currentUser.uid,
+    },
+  });
+  const dogGender = weightData.dog_gender;
+  const dogWeight = weightData.dog_weight;
+  let rer = 70 * Math.pow(dogWeight, 0.75);
+  let der;
+  if (dogGender == 'NONE') {
+    der = 1.6 * rer;
+  } else {
+    der = 1.8 * rer;
+  }
+
+  req.currentData = {
+    reAmount: der,
+  };
+
   next();
 };
 
 module.exports = {
   createIntakeData: createIntakeData,
   readIntakeData: readIntakeData,
-  calcIntake: calcIntake,
+  calcIntakeSum: calcIntakeSum,
+  calcRecommendIntake: calcRecommendIntake,
 };
